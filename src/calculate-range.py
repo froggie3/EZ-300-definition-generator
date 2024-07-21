b76b7c3b2ca3749a62d60e1fd57113fc61f392f0
froggie3 <yokkin.h@gmail.com>
2024-07-21 02:06:51 +0000

    add test

--- src/calculate-range.py
+++ src/calculate-range.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
 
 import sys
+import itertools
+
+
+def readlines_wrapper(lines):
+    """
+    >>> "\\n".join(readlines_wrapper(['9999 aaa\\n']))
+    ''
+    >>> "\\n".join(readlines_wrapper(['9999 aaa\\n', '19998 bbb\\n', '29997 ccc\\n']))
+    '9999\\t19998\\taaa\\n19998\\t29997\\tbbb'
+    """
+    for couple_lines in itertools.pairwise(lines):
+        current, next = couple_lines
+        current_start, *ret = current.split()
+        next_start, *_ = next.split()
+        # print(current_start)
+        yield "\t".join([current_start, next_start, *ret])
 
 
 def main():
     lines = sys.stdin.readlines()
-
-    for couple_lines in zip(lines, lines[1::]):
-        current, next = couple_lines
-        current_start, *ret = current.split()
-        next_start, *_ = next.split()
-
-        # print(current_start)
-        print(*[current_start, next_start, *ret], sep="\t")
+    print("\n".join(readlines_wrapper(lines)))
 
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     main()
